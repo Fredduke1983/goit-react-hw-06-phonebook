207614707c3f15e6b2946c68562972563fbefb06
Nechai_Ihor <fredduke1983@gmail.com>
2023-04-30 16:44:23 +0300

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,27 +2,7 @@ import { ContactForm } from './ContactForm/ContactForm';
 import { ContactFilter } from './ContactFilter/ContactFilter';
 import { PhoneBookStyle } from './app.styled';
 
-// let firstRunStorage = true;
-
 export function App() {
-  // const [filter, setFilter] = useState('');
-
-  // useEffect(() => {
-  //   if (localStorage.getItem('contacts') && firstRunStorage) {
-  //     firstRunStorage = false;
-
-  //     const storage = JSON.parse(localStorage.getItem('contacts'));
-  //     setContacts([...storage]);
-  //   }
-
-  //   localStorage.setItem('contacts', JSON.stringify(contacts));
-  // }, [contacts]);
-
-  // function resetForm() {
-  //   setWriteName('');
-  //   setNumber('');
-  // }
-
   return (
     <PhoneBookStyle>
       <h1>Phonebook</h1>
